daf675f90775826338f5524f0b8bfcfda29424eb
Michael Dearman <mickeyuk@live.co.uk>
2017-06-08 01:27:38 +0100

    First commit

---
+++ Assets/Scripts/FixedCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedCamera : MonoBehaviour {
+
+    public Transform target;
+    public float smoothTime = 0.3F;
+
+    private Vector3 velocity = Vector3.zero;
+
+    // Update is called once per frame
+    void Update () {
+
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(target.position.x - 30, target.position.y, target.position.z - 30), ref velocity, smoothTime);
+
+    }
+}
---
+++ Assets/Scripts/IsometricCharacterController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsometricCharacterController : MonoBehaviour {
+
+    public float walkSpeed = 4f;
+
+    Vector3 forward, right;
+    private float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+
+        // -45 degrees from the world x axis
+        right = Quaternion.Euler(new Vector3(0,90,0)) * forward;
+
+        // Initial speed
+        moveSpeed = walkSpeed;
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Movement
+        if (Input.anyKey) {
+            Move();
+        }
+
+	}
+
+    void Move() {
+
+        // Movement speed
+        Vector3 rightMovement = right * moveSpeed * Input.GetAxis("Horizontal");
+        Vector3 upMovement = forward * moveSpeed * Input.GetAxis("Vertical");
+
+        // Calculate what is forward
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+        // Set new position
+        Vector3 newPosition = transform.position;
+        newPosition += rightMovement;
+        newPosition += upMovement;
+
+        // Smoothly move the new position
+        transform.forward = heading;
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime);
+
+    }
+}
